58b1141401a234308308d7508d66869a961d885f
topoden <topoden@gmail.com>
2024-11-10 09:56:34 +0100

    Add setup function
    And allow to configure filename

--- lua/todo.lua
+++ lua/todo.lua
@@ -10,7 +10,7 @@ local move_today = function()
   vim.cmd("nohlsearch")
 end
 
-M.enable = function()
+M.add = function()
   vim.fn.matchadd("@markup.heading", "\\<INBOX\\>")
   vim.fn.matchadd("@markup.heading", "\\<TODAY\\>")
   vim.fn.matchadd("@markup.heading", "\\<SOON\\>")
@@ -19,9 +19,30 @@ M.enable = function()
   vim.keymap.set('n', 'mt', move_today)
 end
 
-M.disable = function()
+M.remove = function()
   vim.keymap.del('n', 'ms')
   vim.keymap.del('n', 'mt')
+end
+
+M.state = {}
+
+local set_command = function(filename, current, event, callback)
+  if current ~= nil then
+    vim.api.nvim_del_autocmd(current)
+  end
+  return vim.api.nvim_create_autocmd({ event }, {
+    pattern = filename,
+    callback = callback,
+  })
+end
+
+M.setup = function(opts)
+  if M.state.filename == opts.filename then
+    return
+  end
+  M.state.filename = opts.filename
+  M.state.add = set_command(M.state.filename, M.state.add, "BufEnter", function() require("todo").add() end)
+  M.state.rem = set_command(M.state.filename, M.state.rem, "BufLeave", function() require("todo").remove() end)
 end
 
 return M
